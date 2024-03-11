a80323b0342a5800f7ba140d2ebe81e174b88fb5
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-03-12 00:38:27 +0530

    build_and_test step added in status check (broken code)

--- src/test/java/rest/notesapi/tests/NotesApiCommonTests.java
+++ src/test/java/rest/notesapi/tests/NotesApiCommonTests.java
@@ -31,7 +31,7 @@ public class NotesApiCommonTests {
 		Response response = CommonResponse.checkHealthOfNotesApi();
 		response.then().log().body();
 		Assert.assertEquals(response.statusCode(), 200);
-		response.then().body("success", equalTo(true));
+		response.then().body("success", equalTo(false)); // Intentionally done to break the tests
 		response.then().body("status", equalTo(400)); // Intentionally done to break the tests
 		response.then().body("message", equalTo(SUCCESS_HEALTH_MESSAGE));
 		response.then().assertThat().body(matchesJsonSchemaInClasspath("notes-health-check.json"));
